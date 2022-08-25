036d0737b2d9d279761043d11f0e4cce4d18a5af
Igli <iglikoxha@outlook.com>
2022-08-25 13:16:37 +0200

    add gitsync example

--- examples/git-sync/main.tf
+++ examples/git-sync/main.tf
@@ -7,5 +7,8 @@ module "gitsync_airflow" {
   airflow_gitsync_enabled = true
   airflow_gitsync_repo    = "git@github.com:data-max-hq/example-dags.git"
   airflow_gitsync_subpath = "dags"
+  
+  # Supply the base64 encrypted private key to Terraform by using the environment 
+  # variable TF_VAR_airflow_ssh_secret.
   airflow_ssh_secret      = var.airflow_ssh_secret
 }
